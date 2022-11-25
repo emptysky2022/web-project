760d857f2337a21b14cccee11cde250bc670114f
emptysky2022 <emptysky2022@gmail.com>
2022-11-25 14:59:08 +0900

    edit goods list

--- first_project/scripts/shop.js
+++ first_project/scripts/shop.js
@@ -1,8 +1,14 @@
 $(function() {
-    $("li").mouseover(function () { 
-        $(this).css({
-            border: value
-            
-        })
-    })
+    $("#running").click(function () { 
+        // const state = { 'something': $(this).text()}
+        // const title = "test something"
+        // const url = "something.html"
+        // console.log($(this).text())
+        // history.pushState(state, title, url)
+        location.href = "something.html"
+    });
 })
+
+// window.onpopstate = function(e){
+//     alert("location: " + document.location + ", state: " + JSON.stringify(e.state))
+// }

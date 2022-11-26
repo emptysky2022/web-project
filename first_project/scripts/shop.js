f0592d0b4ce1afecd295f22ca423afa1ae3120fc
emptysky2022 <emptysky2022@gmail.com>
2022-11-26 22:05:14 +0900

    create goods list with json

--- first_project/scripts/shop.js
+++ first_project/scripts/shop.js
@@ -1,12 +1,29 @@
 $(function() {
-    $("#running").click(function () { 
-        // const state = { 'something': $(this).text()}
-        // const title = "test something"
-        // const url = "something.html"
-        // console.log($(this).text())
-        // history.pushState(state, title, url)
-        location.href = "something.html"
-    });
+    const requestURL = '../data/list.json'
+    const request = new XMLHttpRequest()
+    request.open('get', requestURL)
+    request.responseType = 'json'
+    request.send()
+    request.onload = function(){
+        const list = request.response
+        let listHtml = ''
+        for(i = 0; i < list.length; i++){
+            listHtml += 
+            "<div id='"+list[i].id+"' class='list'>"
+            + "<img src='../images/"+list[i].img+"' alt='"+list[i].p+"'>"
+            + "<p>" + list[i].p + "</p>"
+            +"</div>"
+        }
+        $(".content").html(listHtml)
+        $(".list").click(function () { 
+            // const state = { 'something': $(this).text()}
+            // const title = "test something"
+            // const url = "something.html"
+            // console.log($(this).text())
+            // history.pushState(state, title, url)
+            location.href = "something.html?id=" + $(this).attr("id")
+        });
+    }
 })
 
 // window.onpopstate = function(e){
--- first_project/scripts/something.js
+++ first_project/scripts/something.js
@@ -1,3 +1,22 @@
 $(function () {
-    $("#test").text()
+    const goodsID = new URL(window.location).searchParams.get('id')
+    let goodHtml
+    const requestURL = '../data/list.json'
+    const request = new XMLHttpRequest()
+    request.open('get', requestURL)
+    request.responseType = 'json'
+    request.send()
+    request.onload = function(){        
+        const list = request.response.filter(e => e.id == goodsID)
+        goodHtml = 
+        "<img class='toolImage' src='../images/"+list[0].img + "' alt='" + list[0].p + "'>"
+        + "<article clas='explan'>"
+        + "<div class='explan_text'>"
+        + "<h1>" + list[0].p +"</h1>"
+        + "<h3>난이도: 5.0/5.0</h3>"
+        + "<p>많이 볼 수 있던 운동기구이다.</p>"
+        + "</div>"
+        + "</article>"
+        $("section").html(goodHtml)
+    }
 })

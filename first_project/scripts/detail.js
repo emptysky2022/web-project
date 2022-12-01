0bf155c947b41316f473a4012b702bcae8eeb90f
emptysky2022 <emptysky2022@gmail.com>
2022-12-01 23:32:14 +0900

    integrated source

--- first_project/scripts/detail.js
+++ first_project/scripts/detail.js
@@ -1,6 +1,6 @@
 $(function () {
-    const goodsID = new URL(window.location).searchParams.get('id')
-    let goodHtml
+    const listId = new URL(window.location).searchParams.get('id')
+    let listHtml
     const requestURL = '../../data/list.json'
     const request = new XMLHttpRequest()
     request.open('get', requestURL)
@@ -8,11 +8,12 @@ $(function () {
     request.send()
     request.onload = function(){
         const listInfo = $("section").attr("id")        
-        const list = request.response[listInfo].filter(e => e.id == goodsID)
+        const list = request.response[listInfo].filter(e => e.id == listId)
         const tagHref = Object.keys(list[0].detail.tag)
         const tagName = Object.values(list[0].detail.tag)
-        goodHtml = 
-        "<img class='toolImage' src='../../images/"+list[0].img + "' alt='" + list[0].name + "'>"
+        console.log(listInfo)
+        listHtml = 
+        "<img class='toolImage' src='../../images/list/"+list[0].img + "' alt='" + list[0].name + "'>"
         + "<article class='explan'>"
         + "<div class='explan_text'>"
         + "<h1>" + list[0].name + " (" +list[0].detail.name+ ")</h1>"
@@ -20,9 +21,20 @@ $(function () {
         + "<div id='tags' class='tags'></div>"
         + "</div>"
         + "</article>"
-        $("section").html(goodHtml)
-        for(let i = 0; i < tagHref.length; i++){
-            $("#tags").append("<a href='" + tagHref[i] + "'>#" + tagName[i]+ "</a>")
+        $("section").html(listHtml)
+        switch(listInfo){
+            case 'goods': {
+                for(let i = 0; i < tagHref.length; i++){
+                    $("#tags").append("<a href='../Exercise/main.html?id=" + tagName[i] + "'>#" + tagName[i]+ "</a>")
+                }
+                break;
+            }
+            case 'exercise': {
+                for(let i = 0; i < tagHref.length; i++){
+                    $("#tags").append("<a href='../Parts/main.html?id=" + tagName[i] + "'>#" + tagName[i]+ "</a>")
+                }
+                break;
+            }
         }
     }
 })
--- first_project/scripts/list.js
+++ first_project/scripts/list.js
@@ -1,10 +1,20 @@
+function pushRandomList(list, listInfo){
+    const listRandom = (listInfo == "exercise") ? parseInt(Math.random() * (list.length - 1)) : parseInt(Math.random() * list.length)
+    const tagRandom = parseInt(Math.random() * list[listRandom].tag.length)
+    $("article").append("<p>이런건 어떠신가요? #" + list[listRandom].tag[tagRandom] + "</p>")
+    $("article").append("<div class='swiper mySwiper'><div class='content swiper-wrapper'></div></div>")
+    pushTagList(list, list[listRandom].tag[tagRandom])
+    $(".mySwiper:last-child").append("<div class='swiper-button-next'></div>" 
+    + "<div class='swiper-button-prev'></div>"
+    + "<div class='swiper-pagination'></div>")
+    pushSwiper()
+}
 function pushTagList(list, searchTags) {  
     for(i = 0; i < list.length; i++){
         if(jQuery.inArray(searchTags, list[i].tag) != -1){
-            console.log(list[i].tag)
             listHtml = 
             "<div id='"+list[i].id+"' class='list swiper-slide'>"
-            + "<img src='../../images/"+list[i].img+"' alt='"+list[i].name+"'>"
+            + "<img src='../../images/list/"+list[i].img+"' alt='"+list[i].name+"'>"
             + "<p>" + list[i].name + "</p>"
             +"</div>"
             $(".swiper-wrapper:last-child").append(listHtml)
@@ -51,6 +61,7 @@ function pushSwiper() {
 }
 
 $(function() {
+    const listId = new URL(window.location).searchParams.get('id')
     let listHtml = ''
     const requestURL = '../../data/list.json'
     const request = new XMLHttpRequest()
@@ -60,27 +71,22 @@ $(function() {
     request.onload = function(){
         const listInfo = $("section").attr("id")
         const list = request.response[listInfo]
-        const listRandom = (listInfo == "exercise") ? parseInt(Math.random() * (list.length - 1)) : parseInt(Math.random() * list.length)
-        const tagRandom = parseInt(Math.random() * list[listRandom].tag.length)
-        $("article").append("<p>이런건 어떠신가요? #" + list[listRandom].tag[tagRandom] + "</p>")
-        $("article").append("<div class='swiper mySwiper'><div class='content swiper-wrapper'></div></div>")
-        console.log("listRandom = " + listRandom + ", list[listRandom].tag = " + list[listRandom].tag.length)
-        pushTagList(list, list[listRandom].tag[tagRandom])
-        $(".mySwiper:last-child").append("<div class='swiper-button-next'></div>" 
-        + "<div class='swiper-button-prev'></div>"
-        + "<div class='swiper-pagination'></div>")
-        pushSwiper()
-        
+        pushRandomList(list, listInfo)
+        pushRandomList(list, listInfo)
+        $("body").append('<button id = "toTop">TOP</button>')
+        $("#toTop").click(function() {   
+            $('html, body').animate({
+                scrollTop : 0    // 0 까지 animation 이동합니다.
+            }, 400);          // 속도 400
+                return false;
+        });
+
         $("#search_check").click(function(){
-            if(!$("#search").val()){
-                $("#search_tag").empty().append("검색창에 태그를 입력해주세요.").show()
-            }
-            else{
+            if($("#search").val()){
                 let searchTags = $("#search").val()
-                $("#search_tag").empty().append("#" + searchTags).show()
                 let list = request.response[listInfo]
                 $("article>p").remove()
-                $(".mySwiper").remove();
+                $(".mySwiper").remove()
                 $("article").append("<p>다음에 대한 검색결과입니다. #" + searchTags+ "</p>")
                 $("article").append("<div class='swiper mySwiper'><div class='content swiper-wrapper'></div></div>")
                 pushTagList(list, searchTags)
@@ -90,23 +96,25 @@ $(function() {
                 pushSwiper()
             }
         })
-        
+        if(listId != null){
+            $("#search").val(listId)
+            $("#search_check").trigger("click")
+        }
     }
     $(window).scroll(function(){
+        if($(document).scrollTop() > 50){
+            $("#toTop").fadeIn();
+            var pagesize =  $( document ).height() - $( window ).height();
+            var percent = parseInt((($(this).scrollTop() / pagesize) * 100));
+            document.getElementById("toTop").innerHTML =  "<h4>↑</h4>"+ percent + "%";
+        }else{
+            $("#toTop").fadeOut()
+        }
         if(!$("#search").val()){
-            if(Math.ceil($(window).scrollTop() + $(window).height() >= $(document).height() - 50) || !($(document).hasScrollBar())){
+            if(Math.ceil($(window).scrollTop() + $(window).height() >= $(document).height() - 50)){
                 const listInfo = $("section").attr("id")
                 const list = request.response[listInfo]
-                const listRandom = (listInfo == "exercise") ? parseInt(Math.random() * (list.length - 1)) : parseInt(Math.random() * list.length)
-                const tagRandom = parseInt(Math.random() * list[listRandom].tag.length)
-                $("article").append("<p>이런건 어떠신가요? #" + list[listRandom].tag[tagRandom] + "</p>")
-                $("article").append("<div class='swiper mySwiper'><div class='content swiper-wrapper'></div></div>")
-                console.log("listRandom = " + listRandom + ", list[listRandom].tag = " + list[listRandom].tag.length)
-                pushTagList(list, list[listRandom].tag[tagRandom])
-                $(".mySwiper:last-child").append("<div class='swiper-button-next'></div>" 
-                + "<div class='swiper-button-prev'></div>"
-                + "<div class='swiper-pagination'></div>")
-                pushSwiper()
+                pushRandomList(list, listInfo)
             }
         }
     })
